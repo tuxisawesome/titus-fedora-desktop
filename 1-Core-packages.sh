c260b58bd6896c6736722520f356049f4f2cf02d
tuxisawesome <67063826+tuxisawesome@users.noreply.github.com>
2022-04-19 18:15:58 -0700

    edit

--- 1-Core-packages.sh
+++ 1-Core-packages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "Installing sddm..."
+echo "Updating system..."
 sudo dnf -y update
+echo "Installing essential tools..."
 sudo dnf -y install sddm git vim wget
 sudo systemctl enable sddm
 sudo systemctl set-default graphical.target
