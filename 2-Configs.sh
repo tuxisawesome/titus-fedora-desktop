742c3ebccde5ee89ba5b0c60d370203aecbe459d
tuxisawesome <67063826+tuxisawesome@users.noreply.github.com>
2022-04-20 13:42:24 -0400

    Optomizing Configs

--- 2-Configs.sh
+++ 2-Configs.sh
@@ -1,26 +1,57 @@
 #!/bin/bash
-
+# 2 - Configs.sh
 echo "Installing configs..."
+if [ "$1" == "-nctt" ]; then
+    cd titus-fedora-desktop
+    mkdir ~/.config
+    cp bg.jpg ~/.config
+    mv dotconfig/* ~/.config/
+    # More packages
+    echo "Installing more packages..."
+    sudo dnf -y install bspwm dconf-editor kitty picom polybar rofi sxhkd thunar nitrogen
+    # Installing ocs-url
+    mv -n .* ~
+    cd rpm-packages
+    sudo dnf -y install ocs-url*.rpm
+    cd ../../
+    echo "Installing SDDM"
+    sudo bash /home/*/titus-fedora-desktop/2.1-Sudo.sh
+    echo "Installing FiraCode and Meslo nerd fonts."
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+    mkdir ~/.fonts
+    mv *.zip ~/.fonts/
+    cd ~/.fonts/
+    unzip FiraCode.zip
+    unzip Meslo.zip
+    sudo fc-cache -vf
+    cd
+fi
+if [ "$1" == "-ctt" ]; then
+    git clone https://github.com/christitustech/fedora-titus
+    cd fedora-titus
+    mkdir ~/.config
+    cp bg.jpg ~/.config
+    mv dotconfig/* ~/.config/
+    # More packages
+    echo "Installing more packages..."
+    sudo dnf -y install bspwm dconf-editor kitty picom polybar rofi sxhkd thunar nitrogen
+    # Installing ocs-url
+    mv -n .* ~
+    cd rpm-packages
+    sudo dnf -y install ocs-url*.rpm
+    cd ../../
+    echo "Installing SDDM"
+    sudo bash /home/*/titus-fedora-desktop/2.1-Sudo.sh
+    echo "Installing FiraCode and Meslo nerd fonts."
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+    mkdir ~/.fonts
+    mv *.zip ~/.fonts/
+    cd ~/.fonts/
+    unzip FiraCode.zip
+    unzip Meslo.zip
+    sudo fc-cache -vf
+    cd
+fi
 
-git clone https://github.com/christitustech/fedora-titus
-cd fedora-titus
-mkdir ~/.config
-cp bg.jpg ~/.config
-mv dotconfig/* ~/.config/
-echo "Installing more packages..."
-sudo dnf -y install bspwm dconf-editor kitty picom polybar rofi sxhkd thunar nitrogen
-
-mv -n .* ~
-cd rpm-packages
-sudo dnf -y install ocs-url*.rpm
-cd ../../
-sudo bash /home/*/titus-fedora-desktop/2.1-Sudo.sh
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
-mkdir ~/.fonts
-mv *.zip ~/.fonts/
-cd ~/.fonts/
-unzip FiraCode.zip
-unzip Meslo.zip
-sudo fc-cache -vf
-cd
--- start.sh
+++ start.sh
@@ -3,10 +3,14 @@ echo "Chris Titus' Fedora Desktop"
 echo "Walter Brobson, 2022"
 
 bash 1-Core-packages.sh
-bash 2-Configs.sh
+if [ "$2" == "-ctt"]; then
+    bash 2-Configs.sh -ctt;
+fi
+if [ "$2" == "-norm"]; then
+    bash 2-Configs.sh -nctt;
+fi
 bash 3-Other-packages.sh
 if [ "$1" == "-n" ]; then
-    bash 4.1-Cleaning-up-noreboot.sh;
-    exit
+    bash 4.1-Cleaning-up-noreboot.sh && exit;
 fi
 bash 4-Cleaning-up.sh
