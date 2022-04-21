d8a786dfa992e0ec72034c5e5a1f7620b42321a1
tuxisawesome <67063826+tuxisawesome@users.noreply.github.com>
2022-04-20 21:33:36 -0400

    gogogogogo

--- start.sh
+++ start.sh
@@ -3,12 +3,14 @@ echo "Chris Titus' Fedora Desktop"
 echo "Walter Brobson, 2022"
 
 bash 1-Core-packages.sh
+
 if [ "$2" == "-ctt"]; then
     bash 2-Configs.sh -ctt;
 fi
 if [ "$2" == "-norm"]; then
     bash 2-Configs.sh -nctt;
 fi
+
 bash 3-Other-packages.sh
 if [ "$1" == "-n" ]; then
     bash 4.1-Cleaning-up-noreboot.sh && exit;
