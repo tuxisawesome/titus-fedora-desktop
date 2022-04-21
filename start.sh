22132551f130e35e0d763652e3e30a450436aeeb
tuxisawesome <67063826+tuxisawesome@users.noreply.github.com>
2022-04-20 21:42:14 -0400

    Go Go Go Go

--- start.sh
+++ start.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 echo "Chris Titus' Fedora Desktop"
 echo "Walter Brobson, 2022"
+while getopts n:nr: flag
+do
+    case "${flag}" in
+        n) normal=${OPTARG};;
+        nr) noreboot=${OPTARG};;
+    esac
+done
+
+echo "Selected with flags:"
+echo "Mode: $normal"
+echo "No Reboot: $noreboot"
+echo ""
+echo "5 seconds to exit, if nessesary..."
+
+sleep 5
 
 bash 1-Core-packages.sh
 
-if [ "$2" == "-ctt"]; then
+if [ "$normal" == "cttconf"]; then
     bash 2-Configs.sh -ctt;
 fi
-if [ "$2" == "-norm"]; then
+if [ "$normal" == "yes"]; then
     bash 2-Configs.sh -nctt;
 fi
 
 bash 3-Other-packages.sh
-if [ "$1" == "-n" ]; then
+if [ "$noreboot" == "y" ]; then
     bash 4.1-Cleaning-up-noreboot.sh && exit;
 fi
 bash 4-Cleaning-up.sh
