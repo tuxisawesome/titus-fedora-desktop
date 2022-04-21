b7b7cfe99dea72ccdc079d28cb7fe08de0c97f81
tuxisawesome <67063826+tuxisawesome@users.noreply.github.com>
2022-04-21 08:38:06 -0400

    sudo

--- 3-Other-packages.sh
+++ 3-Other-packages.sh
@@ -1,2 +1,2 @@
 echo "Installing extra packages from file..."
-dnf -y install fontawesome-fonts fontawesome-fonts-web firefox arandr
+sudo dnf -y install fontawesome-fonts fontawesome-fonts-web firefox arandr
