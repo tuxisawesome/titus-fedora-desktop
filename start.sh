4100537c06586cd4d9761879f9272e65fe5b6ace
tuxisawesome <67063826+tuxisawesome@users.noreply.github.com>
2022-04-21 08:10:12 -0400

    Testing -do not pull

--- start.sh
+++ start.sh
@@ -16,7 +16,11 @@ echo ""
 echo "5 seconds to exit, if nessesary..."
 echo ""
 sleep 5
-
+if $normal == "yes"
+then
+    echo "If statement working";
+fi
+exits
 bash 1-Core-packages.sh
 
 if $normal == "cttconf"
